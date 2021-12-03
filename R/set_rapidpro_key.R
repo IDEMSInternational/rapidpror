3f7d0cef7be4bb656e50d6939d4202f810e70879
lilyclements <lclements1004@outlook.com>
2021-12-03 12:17:06 +0000

    adding error message

--- R/set_rapidpro_key.R
+++ R/set_rapidpro_key.R
@@ -5,5 +5,8 @@
 #' @return
 #' @export
 set_rapidpro_key = function(key) {
+  if (!is.character(key)){
+    stop("`key` provided should be a character variable")
+  }
   pkg_env$rapidpro_key <- key 
 }

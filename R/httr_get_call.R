9ec98619f0c186f6daca1890126b3cf7eee50d15
lilyclements <lclements1004@outlook.com>
2021-12-03 12:25:05 +0000

    error for httr_get_call

--- R/httr_get_call.R
+++ R/httr_get_call.R
@@ -7,6 +7,13 @@
 #' @return
 #' @export
 httr_get_call <- function(get_command, token = get_rapidpro_key()){
+  if (is.null(token)){
+    stop("`token` is NULL. Set token with `set_rapidpro_key`.")
+    # could there be a case where the key isn't needed?
+  }
+  if (is.null(get_command)){
+    stop("`get_command` is NULL. Expecting a website.")
+  }
   response <- httr::GET(get_command, config = httr::add_headers(Authorization = paste("Token", token)))
   raw <- httr::content(response, as = "text")
   results <- jsonlite::fromJSON(raw)

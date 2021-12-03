b3ff734d8afa0ecf0114c7e42d04b04daebaddbd
lilyclements <lclements1004@outlook.com>
2021-12-03 13:35:56 +0000

    Update get_rapidpro_uuid_names.R

--- R/get_rapidpro_uuid_names.R
+++ R/get_rapidpro_uuid_names.R
@@ -1,6 +1,6 @@
 #' Get the UUID names from the environment
-#'
-#' @return returns the UUID names set in the environment for the rapidPro data
+#' @description Call the UUID names from the environment. This set in the function `set_rapidpro_uuid_names`. 
+#' @return returns the UUID names set in the environment for the RapidPro data.
 #' @export
 get_rapidpro_uuid_names = function(){
   get("rapidpro_uuid_names", envir = pkg_env)

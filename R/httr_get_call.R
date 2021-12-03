8c091a793eb2a94a95c0f2e6bcb27405feefc514
lilyclements <lclements1004@outlook.com>
2021-12-03 12:33:53 +0000

    Update httr_get_call.R

--- R/httr_get_call.R
+++ R/httr_get_call.R
@@ -8,11 +8,11 @@
 #' @export
 httr_get_call <- function(get_command, token = get_rapidpro_key()){
   if (is.null(token)){
-    stop("`token` is NULL. Set token with `set_rapidpro_key`.")
+    stop("token is NULL. Set token with `set_rapidpro_key`.")
     # could there be a case where the key isn't needed?
   }
   if (is.null(get_command)){
-    stop("`get_command` is NULL. Expecting a website.")
+    stop("get_command is NULL. Expecting a website.")
   }
   response <- httr::GET(get_command, config = httr::add_headers(Authorization = paste("Token", token)))
   raw <- httr::content(response, as = "text")

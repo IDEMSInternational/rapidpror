ba36335f683a7db03342f8419bf3394d9a9ce747
lilyclements <lclements1004@outlook.com>
2021-12-03 13:32:16 +0000

    Update get_rapidpro_site.R

--- R/get_rapidpro_site.R
+++ R/get_rapidpro_site.R
@@ -1,6 +1,7 @@
-#' Get the site value from the environment
+#' Get the Website from the Environment
+#' @description Call the website from the environment. The website is set in the function `set_rapidpro_website`. 
 #'
-#' @return returns a string containing the website name set in the environment for the rapidPro data.
+#' @return returns a string containing the website name set in the environment for the RapidPro data.
 #' @export
 get_rapidpro_site = function() {
   get("rapidpro_site", envir = pkg_env)

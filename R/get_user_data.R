5d64bfeb54970dab875c2be93c50aa3d3acbd23f
lilyclements <lclements1004@outlook.com>
2021-12-03 12:43:32 +0000

    error messages

--- R/get_user_data.R
+++ R/get_user_data.R
@@ -1,14 +1,28 @@
 #' Get data from each contact
 #'
 #' @param call_type A string containing the call type.
-#' @param rapidpro_site A string containing the rapidpro website to call the data from. See set_rapidpro_site() to amend the website.
-#' @param token A string containing the token to call the data. See set_rapidpro_key() to amend the token.
-#' @param flatten Default FALSE. A boolean denoting whether the data should be flattened into a two-dimensional tabular structure.
+#' @param rapidpro_site A string containing the rapidpro website to call the data from. See `set_rapidpro_site()` to amend the website.
+#' @param token A string containing the token to call the data. See `set_rapidpro_key()` to amend the token.
+#' @param flatten Default `FALSE`. A boolean denoting whether the data should be flattened into a two-dimensional tabular structure.
 #'
 #' @return returns a data frame containing the contact data.
 #' @export
-get_user_data <- function(call_type="contacts.json", rapidpro_site = get_rapidpro_site(), token = get_rapidpro_key(), flatten = FALSE){
-  # todo: checks/error handling messages.
+get_user_data <- function(call_type = "contacts.json", rapidpro_site = get_rapidpro_site(), token = get_rapidpro_key(), flatten = FALSE){
+  if (is.null(rapidpro_site)){
+    stop("rapidpro_site is NULL. Set a website with `set_rapidpro_site`.")
+  }
+  if (is.null(token)){
+    stop("token is NULL. Set a token with `set_rapidpro_key`.")
+  }
+  if (is.null(call_type)){
+    stop("call_type is NULL. Expecting a valid call_type.")
+  }
+  if (!is.character(call_type)){
+    stop("call_type should be a character variable.")
+  }
+  if (!is.logical(flatten)){
+    stop("flatten should be TRUE or FALSE")
+  }
   get_command <- paste(rapidpro_site, call_type, sep = "")
   user_result <- httr_get_call(get_command = get_command, token = token)
   if (flatten){

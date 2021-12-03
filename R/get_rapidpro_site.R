05d11dcf5fcfbc78445602cb38fb009000ad38a0
lilyclements <lclements1004@outlook.com>
2021-12-03 13:34:35 +0000

    Update get_rapidpro_site.R

--- R/get_rapidpro_site.R
+++ R/get_rapidpro_site.R
@@ -1,5 +1,5 @@
-#' Get the Website from the Environment
-#' @description Call the website from the environment. The website is set in the function `set_rapidpro_website`. 
+#' Get the website from the environment
+#' @description Call the website from the environment. The website is set in the function `set_rapidpro_site`. 
 #'
 #' @return returns a string containing the website name set in the environment for the RapidPro data.
 #' @export

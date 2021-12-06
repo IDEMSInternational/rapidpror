7c76260ccde6a4075a567b172bee30ae3213c55d
lilyclements <lclements1004@outlook.com>
2021-12-06 12:51:58 +0000

    Update get_flow_names.R

--- R/get_flow_names.R
+++ R/get_flow_names.R
@@ -2,7 +2,6 @@
 #'
 #' @description Call the names of each run (flow) and their UUID values. This function is often called inside another function.
 #' 
-#' @param call_type A string containing the call type.
 #' @param rapidpro_site A string containing the rapidpro website to call the data from. See `set_rapidpro_site()` to amend the website.
 #' @param token A string containing the token to call the data. See `set_rapidpro_key()` to amend the token.
 #' @param flatten Default `FALSE`. A boolean denoting whether the data should be flattened into a two-dimensional tabular structure.

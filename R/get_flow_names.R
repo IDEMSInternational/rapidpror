f2926d412cc7e75594deedfab8b8f9e92fcf18bd
lilyclements <lclements1004@outlook.com>
2021-12-06 12:48:36 +0000

    Update get_flow_names.R

--- R/get_flow_names.R
+++ R/get_flow_names.R
@@ -9,26 +9,6 @@
 #'
 #' @return A data frame containing the flows data in RapidPro.
 #' @export
-get_flow_names <- function(call_type = "flows.json", rapidpro_site = get_rapidpro_site(), token = get_rapidpro_key(), flatten = FALSE){
-  if (is.null(rapidpro_site)){
-    stop("rapidpro_site is NULL. Set a website with `set_rapidpro_site`.")
-  }
-  if (is.null(token)){
-    stop("token is NULL. Set a token with `set_rapidpro_key`.")
-  }
-  if (is.null(call_type)){
-    stop("call_type is NULL. Expecting a valid call_type.")
-  }
-  if (!is.character(call_type)){
-    stop("call_type should be a character variable.")
-  }
-  if (!is.logical(flatten)){
-    stop("flatten should be TRUE or FALSE")
-  }
-  get_command <- paste(rapidpro_site, call_type, sep = "")
-  flow_names <- httr_get_call(get_command = get_command, token = token)
-  if (flatten){
-    flow_names <- jsonlite::flatten(flow_names)
-  }
-  return(flow_names)
+get_flow_names <- function(rapidpro_site = get_rapidpro_site(), token = get_rapidpro_key(), flatten = FALSE){
+  get_data_from_rapidpro_api(call_type = "flows.json", rapidpro_site = rapidpro_site, token = token, flatten = flatten)
 }

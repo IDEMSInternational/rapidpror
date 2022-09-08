0731cc4d30853d83ba2f82f632c8d4e7e7899150
lilyclements <lily@idems.international>
2022-09-08 12:16:10 +0300

    update rapidpro key

--- R/set_rapidpro_key.R
+++ R/set_rapidpro_key.R
@@ -4,11 +4,11 @@
 #' @param key A string denoting the API Token associated with the website given in `set_rapidpro_site`.
 #' If `file = FALSE`, the string is the key value itself.
 #' If `file = TRUE`, the string is a text file containing the key. Only the first line will be taken in the text file.
-#' @param file A boolean denoting whether the key given is a file path or a string
+#' @param file A boolean denoting whether the key given is a file path or a string.
 #'
 #' @return A string containing the key (token) required to access the given RapidPro website.
 #' @export
-set_rapidpro_key = function(key, file = TRUE) {
+set_rapidpro_key = function(key, file = FALSE) {
   if (file){
     file_value <- read.table(key, quote="\"", comment.char="")
     key <- file_value[[1]]

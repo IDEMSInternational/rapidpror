bb553cfca2f1a34d94b3597ba7fdedd0ed196863
lilyclements <lily@idems.international>
2022-09-09 16:23:11 +0300

    set rapidpro site

--- R/set_rapidpro_site.R
+++ R/set_rapidpro_site.R
@@ -1,10 +1,11 @@
 #' Reset the website in the environment
-#' @description Set the website to access the data.
-#' 
-#' @param site A string denoting the website to call the API data from.
 #'
-#' @return A string containing the RapidPro website.
+#' @param site A string denoting the website associated with the token given in `set_rapidpro_key`.
 #'
+#' @return A string containing the website required to access RapidPro.
+#' @export
+#'
+#' @examples #
 set_rapidpro_site = function(site) {
   if (!is.character(site)){
     stop("`site` provided should be a character variable")

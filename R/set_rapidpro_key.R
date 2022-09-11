64f00407d743c9bb46cf1b36e38ad29b867be5af
lilyclements <lily@idems.international>
2022-09-11 12:25:09 +0300

    adding read.table package

--- R/set_rapidpro_key.R
+++ R/set_rapidpro_key.R
@@ -10,7 +10,7 @@
 #' @export
 set_rapidpro_key = function(key, file = FALSE) {
   if (file){
-    file_value <- read.table(key, quote="\"", comment.char="")
+    file_value <- utils::read.table(key, quote="\"", comment.char="")
     key <- file_value[[1]]
   }
   if (!is.character(key)){

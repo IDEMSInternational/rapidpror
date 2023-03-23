149ebf16a38c1b66942a140afdfc8c0abe826136
lilyclements <lily@idems.international>
2023-03-23 19:02:59 +0000

    undo previous commit (test)

--- R/set_rapidpro_uuid.R
+++
@@ -1,9 +0,0 @@
-#' Set the UUID names in the environment for the flow data
-#' @description Set the UUID names to access data in RapidPro.
-#'
-#' @param uuid_names Default get_flow_names() (recommended). Data frame containing the flow names and UUIDs.
-#'
-#' @return Setting a data frame containing RapidPro flow data to the environment.
-set_rapidpro_uuid <- function(uuid_names = get_flow_names()){
-  pkg_env$rapidpro_uuid_names <- uuid_names 
-}

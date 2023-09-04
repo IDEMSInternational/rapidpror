4df13c1b0c8b76743b14735132af97f6b990c231
lilyclements <lily@idems.international>
2023-09-04 12:25:42 +0100

    Adding handle flows functions

--- R/flow_data_calculation.R
+++ R/flow_data_calculation.R
@@ -1,73 +1,52 @@
-#' Calculation to get the flow data.
-#' @description This is called in the `get_flow_data` function. This function arranges the function for different flow types.
+#' Calculate flow data.
+#'
+#' This function arranges flow data for different flow types.
 #'
 #' @param result_flow Data frame containing flow data.
 #' @param flatten Default `FALSE`. A boolean denoting whether the data should be flattened into a two-dimensional tabular structure.
 #' @param flow_type Default `none`. Takes values `"praise"`, `"calm"`, `"check_in"`, `"tips"`, `"none"`. These are related to ParentText analysis.
 #' @param date_from character string giving the date to filter the data from.
 #' @param date_to character string giving the date to filter the data to.
-#' @param format_date from `as.POSIX*` function: character string giving a date-time format as used by `strptime`.
-#' @param tzone_date from `as.POSIX*` function: time zone specification to be used for the conversion, if one is required. System-specific (see time zones), but "" is the current time zone, and "GMT" is UTC (Universal Time, Coordinated). Invalid values are most commonly treated as UTC, on some platforms with a warning.
+#' @param format_date character string giving a date-time format as used by `strptime`.
+#' @param tzone_date time zone specification to be used for the conversion, if one is required.
 #' @param created_on Parameter for flow_data.
 #'
 #' @return Data frame displaying the flow data.
 #'
 #' @importFrom dplyr %>%
 flow_data_calculation <- function(result_flow, flatten = FALSE, flow_type = "none", date_from = NULL, date_to = NULL,
-                                  format_date = "%Y-%m-%d", tzone_date = "UTC", created_on = FALSE){
-  if (length(result_flow) == 0){
-    flow_interaction <- NULL
-  } else {
-    if (!is.null(date_from)){
-      result_flow <- result_flow %>% dplyr::filter(as.POSIXct(date_from, format=format_date, tzone = tzone_date) < as.POSIXct(result_flow$created_on, format="%Y-%m-%dT%H:%M:%OS", tz = "UTC"))
-    }
-    if (!is.null(date_to)){
-      result_flow <- result_flow %>% dplyr::filter(as.POSIXct(date_to, format=format_date, tzone = tzone_date) > as.POSIXct(result_flow$created_on, format="%Y-%m-%dT%H:%M:%OS", tz = "UTC"))
-    }
-    uuid <- result_flow$contact$uuid
-    interacted <- result_flow$responded
-    created_run_on <- result_flow$created_on
-    exit_type <- result_flow$exit_type
-    modified_on <- result_flow$modified_on
-    exited_on <- result_flow$exited_on
-    
-    # Relevant to ParentText only -----------------------------------------------------
-    # for check in:
-    if (flow_type == "praise" && nrow(result_flow$values) > 0){
-      response <- result_flow$values$praise_interaction$category 
-      if (!is.null(response)){
-        flow_interaction <- tibble::tibble(uuid, interacted, response, created_run_on)
-        response <- tidyr::replace_na(response, "No response")
-      } else {
-        flow_interaction <- tibble::tibble(uuid, interacted, response = "No response", created_run_on) 
-      }
-    } else if (flow_type == "calm" && !is.null(result_flow$values$calm_interaction)){
-      response <- result_flow$values$calm_interaction$category
-      response <- tidyr::replace_na(response, "No response")
-      flow_interaction <- tibble::tibble(uuid, interacted, response, created_run_on)
-    } else if (flow_type == "check_in" && nrow(result_flow$values) > 0){
-      if (is.null(result_flow$values$checkin_managed$category)){
-        managed_to_do_something <- "No response"
-      } else {
-        managed_to_do_something <- result_flow$values$checkin_managed$category
-      }
-      if (is.null(result_flow$values$checkin_how$category)){
-        response <- "No response"
-      } else {
-        response <- result_flow$values$checkin_how$category
-      }
-      flow_interaction <- tibble::tibble(uuid, interacted, managed_to_do_something, response, created_run_on)
-    } else if (flow_type == "tips" && nrow(result_flow$values) > 0){
-      if (is.null(result_flow$values$know_more$category)){
-        category <- "No response"
-      } else {
-        category <- result_flow$values$know_more$category
-      }
-      flow_interaction <- tibble::tibble(uuid, interacted, category, created_run_on)
-    } else {
-      flow_interaction <- tibble::tibble(uuid, interacted, created_run_on)
-    }
+                                   format_date = "%Y-%m-%d", tzone_date = "UTC", created_on = FALSE) {
+  if (length(result_flow) == 0) {
+    return(NULL)
+  }
+  
+  # Filter data based on date range
+  if (!is.null(date_from)) {
+    result_flow <- filter_date_range(result_flow, date_from, format_date, tzone_date)
+  }
+  if (!is.null(date_to)) {
+    result_flow <- filter_date_range(result_flow, date_to, format_date, tzone_date, start = FALSE)
+  }
+  
+  # Extract relevant columns
+  uuid <- result_flow$contact$uuid
+  interacted <- result_flow$responded
+  created_run_on <- result_flow$created_on
+  
+  # Handle different flow types
+  flow_interaction <- switch(flow_type,
+                             "praise" = handle_praise_flow(result_flow),
+                             "calm" = handle_calm_flow(result_flow),
+                             "check_in" = handle_check_in_flow(result_flow),
+                             "check_in_2" = handle_check_in_2_flow(result_flow),
+                             "tips" = handle_tips_flow(result_flow),
+                             default = tibble::tibble(uuid, interacted, created_run_on)
+  )
+  
+  # Flatten the data if requested
+  if (flatten) {
     flow_interaction <- jsonlite::flatten(flow_interaction)
   }
+  
   return(flow_interaction)
 }
---
+++ R/handle_flows.R
@@ -0,0 +1,101 @@
+#' Helper function to filter data by date range.
+#'
+#' This function filters a data frame based on a specified date range.
+#'
+#' @param data A data frame to be filtered.
+#' @param date The date to filter the data by.
+#' @param format The date-time format as used by `strptime`.
+#' @param tzone The time zone specification to be used for conversion.
+#' @param start A logical value indicating whether to filter for dates before or after the specified date.
+#'
+#' @return A filtered data frame.
+#'
+#' @importFrom dplyr %>%
+filter_date_range <- function(data, date, format, tzone, start = TRUE) {
+  date_posix <- as.POSIXct(date, format = format, tz = tzone)
+  if (start) {
+    return(dplyr::filter(data, date_posix < as.POSIXct(data$created_on, format="%Y-%m-%dT%H:%M:%OS", tz = "UTC")))
+  } else {
+    return(dplyr::filter(data, date_posix > as.POSIXct(data$created_on, format="%Y-%m-%dT%H:%M:%OS", tz = "UTC")))
+  }
+}
+
+#' Helper function to handle praise flow data.
+#'
+#' This function processes praise flow data and extracts relevant information.
+#'
+#' @param data A data frame containing praise flow data.
+#'
+#' @return A tibble with relevant information.
+#'
+handle_praise_flow <- function(data) {
+  response <- data$values$praise_interaction$category
+  response <- tidyr::replace_na(response, "No response")
+  return(tibble::tibble(uuid = data$contact$uuid, interacted = data$responded, response, created_run_on = data$created_on))
+}
+
+#' Helper function to handle calm flow data.
+#'
+#' This function processes calm flow data and extracts relevant information.
+#'
+#' @param data A data frame containing calm flow data.
+#'
+#' @return A tibble with relevant information.
+#'
+handle_calm_flow <- function(data) {
+  response <- data$values$calm_interaction$category
+  response <- tidyr::replace_na(response, "No response")
+  return(tibble::tibble(uuid = data$contact$uuid, interacted = data$responded, response, created_run_on = data$created_on))
+}
+
+#' Helper function to handle check-in flow data.
+#'
+#' This function processes check-in flow data and extracts relevant information.
+#'
+#' @param data A data frame containing check-in flow data.
+#'
+#' @return A tibble with relevant information.
+#'
+handle_check_in_flow <- function(data) {
+  managed_to_do_something <- ifelse(is.na(data$values$checkin_managed$category), "No response", data$values$checkin_managed$category)
+  response <- ifelse(is.na(data$values$checkin_how$category), "No response", data$values$checkin_how$category)
+  return(tibble::tibble(uuid = data$contact$uuid, interacted = data$responded, managed_to_do_something, response, created_run_on = data$created_on))
+}
+
+#' Helper function to handle check-in 2 flow data.
+#'
+#' This function processes check-in 2 flow data and extracts relevant information.
+#'
+#' @param data A data frame containing check-in 2 flow data.
+#'
+#' @return A tibble with relevant information.
+#'
+handle_check_in_2_flow <- function(data) {
+  handle_type_flow(data = data, type = "completed")
+}
+
+#' Helper function to handle flow data of a specified type.
+#'
+#' This function processes flow data of a specified type and extracts relevant information.
+#'
+#' @param data A data frame containing flow data.
+#' @param type The type of flow data to handle.
+#'
+#' @return A tibble with relevant information.
+#'
+handle_type_flow <- function(data, type = "completed") {
+  response <- ifelse(is.na(data$values[[type]]$category), "No response", data$values[[type]]$category)
+  return(tibble::tibble(uuid = data$contact$uuid, interacted = data$responded, response, created_run_on = data$created_on))
+}
+
+#' Helper function to handle tips flow data.
+#'
+#' This function processes tips flow data and extracts relevant information.
+#'
+#' @param data A data frame containing tips flow data.
+#'
+#' @return A tibble with relevant information.
+#'
+handle_tips_flow <- function(data) {
+  handle_type_flow(data = data, type = "know_more")
+}

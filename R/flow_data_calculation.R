f4a1e31cc6f9fa2c55a2c83eb20e99904134d6b4
lilyclements <lily@idems.international>
2023-09-04 14:24:28 +0100

    update flows

--- R/flow_data_calculation.R
+++ R/flow_data_calculation.R
@@ -5,6 +5,7 @@
 #' @param result_flow Data frame containing flow data.
 #' @param flatten Default `FALSE`. A boolean denoting whether the data should be flattened into a two-dimensional tabular structure.
 #' @param flow_type Default `none`. Takes values `"praise"`, `"calm"`, `"check_in"`, `"tips"`, `"none"`. These are related to ParentText analysis.
+#' @param flow_handle_type Default `NULL`. If `flow_type` is `other`, this is the value of the flow under `$value` to get the response for.
 #' @param date_from character string giving the date to filter the data from.
 #' @param date_to character string giving the date to filter the data to.
 #' @param format_date character string giving a date-time format as used by `strptime`.
@@ -14,12 +15,17 @@
 #' @return Data frame displaying the flow data.
 #'
 #' @importFrom dplyr %>%
-flow_data_calculation <- function(result_flow, flatten = FALSE, flow_type = "none", date_from = NULL, date_to = NULL,
-                                   format_date = "%Y-%m-%d", tzone_date = "UTC", created_on = FALSE) {
+flow_data_calculation <- function(result_flow, flatten = FALSE, flow_type = "none", flow_handle_type = NULL, date_from = NULL, date_to = NULL,
+                                  format_date = "%Y-%m-%d", tzone_date = "UTC", created_on = FALSE) {
   if (length(result_flow) == 0) {
     return(NULL)
   }
-  
+  if (flow_type == "other"){
+    if (is.null(flow_handle_type)){
+      warning("flow_handle_type is NULL. Setting flow_type as none.")
+      flow_type <- "none"
+    }
+  }
   # Filter data based on date range
   if (!is.null(date_from)) {
     result_flow <- filter_date_range(result_flow, date_from, format_date, tzone_date)
@@ -40,7 +46,8 @@ flow_data_calculation <- function(result_flow, flatten = FALSE, flow_type = "non
                              "check_in" = handle_check_in_flow(result_flow),
                              "check_in_2" = handle_check_in_2_flow(result_flow),
                              "tips" = handle_tips_flow(result_flow),
-                             default = tibble::tibble(uuid, interacted, created_run_on)
+                             "other" = handle_type_flow(result_flow, type = flow_handle_type),
+                             "none" = tibble::tibble(uuid, interacted, created_run_on)
   )
   
   # Flatten the data if requested
--- R/get_flow_data.R
+++ R/get_flow_data.R
@@ -7,7 +7,8 @@
 #' @param token A string containing the token to call the data. See `set_rapidpro_key()` to amend the token.
 #' @param flatten Default `FALSE`. A boolean denoting whether the data should be flattened into a two-dimensional tabular structure.
 #' @param checks Default `FALSE`. A boolean whether to check and update the data if it is not found.
-#' @param flow_type Default `none`. Takes values `"praise"`, `"calm"`, `"check_in"`, `"tips"`, `"none"`. These are related to ParentText.
+#' @param flow_type Default `none`. Takes values `"praise"`, `"calm"`, `"check_in"`, `"tips"`, `"none"`, `"other"`. These are related to ParentText.
+#' @param flow_handle_type Default `NULL`. If `flow_type` is `other`, this is the value of the flow under `$value` to get the response for.
 #' @param include_archived_data Default `FALSE`. A boolean denoting whether to include archived data or not.
 #' @param get_by A string denoting how to access the archived data (if `include_archived_data` is `TRUE`). Options are `"read_from_RDS"`, `"gotit"`. The first denotes that the data is read from an RDS file (given in `read_archived_data_from`), the latter that the data is stored and will be read in directly (see `data_from_archived`).
 #' @param data_from_archived If `get_by = "gotit"`, this is the archived data.
@@ -22,99 +23,110 @@
 #' @export
 # TODO: `flow_type` is relevant to ParentText. We should have a general wrapper for this function in the rapidpror package
 # That wrapper function calls this parenttext specific function from a separate package, and has flow_type = "none".
-get_flow_data <- function(uuid_data = get_rapidpro_uuid_names(), flow_name, rapidpro_site = get_rapidpro_site(), token = get_rapidpro_key(), flatten = FALSE, checks = FALSE, 
-                          flow_type = "none",
-                          include_archived_data = FALSE, get_by = "gotit", data_from_archived = NULL, # was = archived_data
-                          read_archived_data_from = NULL, # was "archived_data_monthly.RDS" 
-                          created_on = FALSE,
-                          date_from = NULL, date_to = NULL, format_date = "%Y-%m-%d", tzone_date = "UTC"){
-  call_type <- "runs.json?flow=" # check no issues have occurred from moving this from function parameters to here.
-  if (is.null(rapidpro_site)){
+get_flow_data <- function (uuid_data = get_rapidpro_uuid_names(), flow_name, 
+                            rapidpro_site = get_rapidpro_site(), token = get_rapidpro_key(), 
+                            flatten = FALSE, checks = FALSE, flow_type = "none", flow_handle_type = NULL, include_archived_data = FALSE, 
+                            get_by = "gotit", data_from_archived = NULL, read_archived_data_from = NULL, 
+                            created_on = FALSE, date_from = NULL, date_to = NULL, format_date = "%Y-%m-%d", 
+                            tzone_date = "UTC") 
+{
+  call_type <- "runs.json?flow="
+  if (is.null(rapidpro_site)) {
     stop("rapidpro_site is NULL. Set a website with `set_rapidpro_site`.")
   }
-  if (is.null(token)){
+  if (is.null(token)) {
     stop("token is NULL. Set a token with `set_rapidpro_key`.")
   }
-  if (is.null(call_type)){
+  if (is.null(call_type)) {
     stop("call_type is NULL. Expecting a valid call_type.")
   }
-  if (!is.character(call_type)){
+  if (!is.character(call_type)) {
     stop("call_type should be a character variable.")
   }
-  if (is.null(flow_name)){
+  if (is.null(flow_name)) {
     stop("flow_name is NULL. Expecting a valid flow_name")
   }
-  if (!is.character(flow_name)){
+  if (!is.character(flow_name)) {
     stop("flow_name should be a character variable.")
   }
-  if (!is.logical(flatten)){
+  if (!is.logical(flatten)) {
     stop("flatten should be TRUE or FALSE")
   }
-  if (!is.logical(checks)){
+  if (!is.logical(checks)) {
     stop("checks should be TRUE or FALSE")
   }
-  
-  if (checks){
+  if (checks) {
     i = 1
-    if (nrow(uuid_data[which(uuid_data$name == flow_name),]) == 0 & i == 1){
+    if (nrow(uuid_data[which(uuid_data$name == flow_name), 
+    ]) == 0 & i == 1) {
       message("flow_name not recognised. Updating uuid_data sheet")
       set_rapidpro_uuid_names()
       uuid_data = get_rapidpro_uuid_names()
       i = i + 1
-      if (nrow(uuid_data[which(uuid_data$name == flow_name),]) == 0){
+      if (nrow(uuid_data[which(uuid_data$name == flow_name), 
+      ]) == 0) {
         stop("flow_name not recognised.")
-      } else {
+      }
+      else {
         message("flow_name recognised. Updated uuid_data sheet")
       }
     }
   }
   flow_data_bank <- NULL
   flow_data <- NULL
-  for (i in flow_name){
+  for (i in flow_name) {
     j <- which(flow_name == i)
-    uuid_flow <- uuid_data[which(uuid_data$name == i),]
-    get_command <- paste(rapidpro_site, call_type, uuid_flow[1], sep = "")
-    result_flow <- httr_get_call(get_command = get_command, token = token)
-    
-    if (length(result_flow) == 0){
+    uuid_flow <- uuid_data[which(uuid_data$name == i), ]
+    get_command <- paste(rapidpro_site, call_type, uuid_flow[1], 
+                         sep = "")
+    result_flow <- rapidpror:::httr_get_call(get_command = get_command, 
+                                             token = token)
+    if (length(result_flow) == 0) {
       flow_data[[j]] <- NULL
     } else {
-      flow_data[[j]] <- flow_data_calculation(result_flow = result_flow, flatten = flatten, flow_type = flow_type, date_from = date_from, date_to = date_to,
-                                              format_date = format_date, tzone_date = tzone_date, created_on = created_on) %>%
-        dplyr::mutate(flow_type = uuid_flow[1,1]) 
+      flow_data[[j]] <- flow_data_calculation2(result_flow = result_flow, 
+                                               flatten = flatten, flow_type = flow_type,
+                                               flow_handle_type = flow_handle_type, date_from = date_from, 
+                                               date_to = date_to, format_date = format_date, 
+                                               tzone_date = tzone_date, created_on = created_on) %>% 
+        dplyr::mutate(flow_type = uuid_flow[1, 1])
     }
   }
-  if (!is.null(flow_data)){
+  if (!is.null(flow_data)) {
     names(flow_data) <- flow_name[1:length(flow_data)]
   }
   flow_data <- plyr::ldply(flow_data)
-  
-  if (!include_archived_data){
+  if (!include_archived_data) {
     flow_data_bank <- flow_data
-  } else {
+  }
+  else {
     flow_data_bank[[1]] <- flow_data
-    if (get_by == "read"){
+    if (get_by == "read") {
       archived_data <- readRDS(read_archived_data_from)
-    } else {
+    }
+    else {
       archived_data <- data_from_archived
     }
     arch_data_bank <- NULL
-    for (i in flow_name){
+    for (i in flow_name) {
       j <- which(flow_name == i)
-      uuid_flow <- uuid_data[which(uuid_data$name == i),]
+      uuid_flow <- uuid_data[which(uuid_data$name == i), 
+      ]
       arch_data <- NULL
-      for (k in 1:length(archived_data)){
-        arch_flow_data_K <- archived_data[[k]] %>% dplyr::filter(archived_data[[k]]$flow$name == i)
-        arch_data[[k]] <- flow_data_calculation(result_flow = arch_flow_data_K, flow_type = flow_type)
+      for (k in 1:length(archived_data)) {
+        arch_flow_data_K <- archived_data[[k]] %>% dplyr::filter(archived_data[[k]]$flow$name == 
+                                                                   i)
+        arch_data[[k]] <- flow_data_calculation(result_flow = arch_flow_data_K, 
+                                                flow_type = flow_type)
       }
       names(arch_data) <- names(archived_data)[1:length(arch_data)]
       arch_data_bank[[j]] <- plyr::ldply(arch_data)
-      arch_data_bank[[j]]$`.id` <- NULL
-      arch_data_bank[[j]] <- arch_data_bank[[j]] %>% dplyr::mutate(flow_type = uuid_flow[1,1])
+      arch_data_bank[[j]]$.id <- NULL
+      arch_data_bank[[j]] <- arch_data_bank[[j]] %>% dplyr::mutate(flow_type = uuid_flow[1, 
+                                                                                         1])
     }
     names(arch_data_bank) <- flow_name
     arch_data_bank <- plyr::ldply(arch_data_bank)
-    
     flow_data_bank[[2]] <- arch_data_bank
     names(flow_data_bank) <- c("Current", "Archived")
     flow_data_bank <- plyr::ldply(flow_data_bank)
